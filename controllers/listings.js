6873c5eeba94bbbad2f15653d51fe37aeec18592
Sunidhi chauhan <chauhansunidhi223@gmail.com>
2024-09-26 17:13:09 +0530

    Add Project Files

---
+++ controllers/listings.js
@@ -0,0 +1,61 @@
+const Listing = require("../models/listing");
+
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  res.render("listings/new.ejs");
+};
+
+module.exports.showListing = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({
+      path: "reviews",
+      populate: {
+        path: "author",
+      },
+    })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing you requested for doesn't exist");
+    res.redirect("/listings");
+  }
+  console.log(listing);
+  res.render("Listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res, next) => {
+  const newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  await newListing.save();
+  req.flash("success", "New listing Created!");
+  res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing you requested for doesn't exist");
+    res.redirect("/listings");
+  }
+  res.render("listings/edit.ejs", { listing });
+};
+
+module.exports.updateListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  req.flash("success", " listing Updated!");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destroyListing = async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  req.flash("success", "listing Deleted!");
+  res.redirect("/listings");
+};
